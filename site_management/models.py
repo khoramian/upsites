f745a835e3e201b3bacbb1b023bf1f5a4d300c99
khoramian <khoramian8@gmail.com>
2020-12-19 14:29:18 -0500

    update

--- site_management/models.py
+++ site_management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 class Websites(models.Model):
+    """
+    Represents a website details
+    """
+    name = models.CharField(max_length=100)
+    URL = models.URLField()
+
+    def __str__(self):
+        return self.name

d75cf8aafc19458613d83a5c5bc8a30fd117b771
khoramian <khoramian8@gmail.com>
2020-12-19 15:07:25 -0500

    update

--- upsites/urls.py
+++ upsites/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from site_management.views import list_websites
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('site_management/list', ),
+    path('site_management/list', list_websites),
 ]

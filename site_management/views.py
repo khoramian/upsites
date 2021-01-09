97038dba71eb9cc866097c16a9f354895dc20417
khoramian <khoramian8@gmail.com>
2021-01-09 10:33:06 -0500

    update

--- site_management/views.py
+++ site_management/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse   # delete ///////////////////////////////////////////
+from django.http import HttpResponse   # delete //////////////////////////////////////////
 from site_management.models import Websites
 
 
